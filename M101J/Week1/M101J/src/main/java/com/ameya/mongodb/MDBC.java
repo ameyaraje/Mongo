b8a1d8ce4e4f4c2291bceb5d83c2e57fe174fe25
ameyaraje <ameyacraje@gmail.com>
2017-08-22 22:47:57 -0700

    modified .gitignore

---
+++ M101J/Week1/M101J/src/main/java/com/ameya/mongodb/MDBC.java
@@ -0,0 +1,11 @@
+package com.ameya.mongodb;
+
+import com.mongodb.MongoClient;
+
+public class MDBC {
+
+	public static void main(String[] args) {
+		MongoClient client = new MongoClient();
+	}
+
+}
--- M101J/Week1/M101J/target/classes/index.ftl
+++
@@ -1,6 +0,0 @@
-<html>
-<title>Some random title here</title>
-<body>
-<h1>Hello ${name}</h1>
-</body>
-</html>
