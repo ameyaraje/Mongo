1cb63b85586c4ced06170647ef56aa695cb61198
ameyaraje <ameyacraje@gmail.com>
2017-08-22 19:50:15 -0700

    Combine FM and spark

--- Week1/M101J/src/main/java/com/ameya/mongodb/FreemarkerStarter.java
+++ Week1/M101J/src/main/java/com/ameya/mongodb/FreemarkerStarter.java
@@ -1,35 +1,71 @@
 package com.ameya.mongodb;
 
-import java.io.File;
-import java.io.StringWriter;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.*;
+import java.util.*;
 
 import freemarker.cache.FileTemplateLoader;
 import freemarker.template.Configuration;
 import freemarker.template.Template;
+import freemarker.template.TemplateException;
 
 public class FreemarkerStarter {
 
 	public static void main(String[] args) {
-		Configuration config = new Configuration();
-		config.setClassForTemplateLoading(FreemarkerStarter.class, "/");
-		
+
+		//Freemarker configuration object
+		Configuration cfg = new Configuration();
 		try {
-//			FileTemplateLoader templateLoader = new FileTemplateLoader(new File("resources"));
-//			config.setTemplateLoader(templateLoader);
-			Template template = config.getTemplate("index.ftl");
-			StringWriter writer = new StringWriter();
-			Map<String, Object> map = new HashMap<>();
-			map.put("name", "Ameya");
+			//Load template from source folder
+			Template template = cfg.getTemplate("index.ftl");
+
+			// Build the data-model
+			Map<String, Object> data = new HashMap<String, Object>();
+			data.put("message", "Hello World!");
+
+			//List parsing 
+			List<String> countries = new ArrayList<String>();
+			countries.add("India");
+			countries.add("United States");
+			countries.add("Germany");
+			countries.add("France");
+
+			data.put("countries", countries);
+
+
+			// Console output
+			Writer out = new OutputStreamWriter(System.out);
+			template.process(data, out);
+			out.flush();
+
 			
-			template.process(map, writer);
-			System.out.println(writer);
-			
-		}
-		catch (Exception e) {
-			System.out.println(e.getMessage());
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (TemplateException e) {
+			e.printStackTrace();
 		}
 	}
+
+
+
+	//	public static void main(String[] args) {
+	//		Configuration config = new Configuration();
+	//		config.setClassForTemplateLoading(FreemarkerStarter.class, "/");
+	//		
+	//		try {
+	////			FileTemplateLoader templateLoader = new FileTemplateLoader(new File("resources"));
+	////			config.setTemplateLoader(templateLoader);
+	//			Template template = config.getTemplate("src/index.ftl");
+	//			StringWriter writer = new StringWriter();
+	//			Map<String, Object> map = new HashMap<>();
+	//			map.put("name", "Ameya");
+	//			
+	//			template.process(map, writer);
+	//			System.out.println(writer);
+	//			
+	//		}
+	//		catch (Exception e) {
+	//			System.out.println(e.getMessage());
+	//		}
+	//	}
 
 }
